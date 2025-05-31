a6e258d3b7cc65c7ab0f4848c52196a7ed92b3a8
mtunuksi <116896894+mtunuksi@users.noreply.github.com>
2025-05-31 10:38:23 +0300

    Update main.R

--- main.R
+++ main.R
@@ -1,8 +1,9 @@
 library(tidyverse)
 library(here)
 
-tv_hours_table <- gss_cat %>% 
-  group_bys(marital) %>% 
+tv_hours_table <- gss_cat %>%
+  filter(age >= 30) %>% 
+  group_by(marital) %>% 
   summarize(mean_tv_hours = mean(tvhours, na.rm = TRUE))
 
 
